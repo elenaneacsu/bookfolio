ccb4e04bd095cffcbd14c8f0b864dc55402f5f91
Elena Neacsu <eneacsu@intralinks.com>
2021-04-18 19:07:45 +0300

    Changed the name of the app

--- app/src/androidTest/java/com/elenaneacsu/bookfolio/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/elenaneacsu/bookfolio/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.elenaneacsu.disertatieapp", appContext.packageName)
+        assertEquals("com.elenaneacsu.bookfolio", appContext.packageName)
     }
 }
